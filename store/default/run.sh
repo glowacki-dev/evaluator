c7f8b75fb63d12e9151918ad7e78ee91b37f0a32
Maciej Głowacki <maciej.glowacki@daftcode.pl>
2017-10-02 00:48:10 +0200

    Format execution time in miliseconds

--- store/default/run.sh
+++ store/default/run.sh
@@ -3,10 +3,10 @@
 exec 1> /code/log
 exec 2> /code/errors
 
-START=$(date +%s.%2N)
+START=$(date +%s%3N)
 
 $1 /code/$2
 
-END=$(date +%s.%2N)
+END=$(date +%s%3N)
 
 echo ${END} - ${START} | bc > /code/time
